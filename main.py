6c23ee7dcd391b35db9566a27f9c4fd4e548d563
Hexqueller <dima.nesed@gmail.com>
2024-09-26 18:58:00 +0300

    Фича - обьединение нескольких файлов

--- main.py
+++ main.py
@@ -5,31 +5,42 @@ import saver
 backup_file = "/etc/hosts.backup"
 host_pattern = re.compile(r'^\s*(\S+)\s+ansible_host\s*=\s*(\S+)$')
 
-if len(sys.argv) != 2:
-    print('Usage: python main.py <path_to_ansible_hosts_file>')
+if len(sys.argv) < 2:
+    print('Usage: python main.py <path_to_ansible_hosts_file1> <path_to_ansible_hosts_file2> ...')
     sys.exit(1)
 
+# Если первый аргумент "restore", то восстанавливаем бэкап и выходим
 if sys.argv[1] == "restore":
     saver.restore(backup_file)
     sys.exit(1)
 
+# Создание резервной копии
 saver.backuper(backup_file)
-path_to_hosts = sys.argv[1]
-with open(path_to_hosts, 'r') as f:
-    hosts_ansible = f.read()
-
-with open(backup_file, 'r') as f:
-    hosts_file = f.read()
 
 hosts_list = []
-for line in hosts_ansible.split('\n'):
-    if line == '' or line.startswith('#'):
+# Обработка всех переданных файлов
+for path_to_hosts in sys.argv[1:]:
+    try:
+        with open(path_to_hosts, 'r') as f:
+            hosts_ansible = f.read()
+    except FileNotFoundError:
+        print(f"File not found: {path_to_hosts}")
         continue
-    match = host_pattern.match(line)
-    if match:
-        hostname = match.group(1)
-        ip = match.group(2)
-        if f"{ip}\t{hostname}" not in hosts_file:
-            hosts_list.append((ip, hostname))
 
-saver.merge_files(hosts_list, path_to_hosts, backup_file)
+    # Чтение текущего содержимого файла /etc/hosts
+    with open(backup_file, 'r') as f:
+        hosts_file = f.read()
+
+    for line in hosts_ansible.split('\n'):
+        if line == '' or line.startswith('#'):
+            continue
+        match = host_pattern.match(line)
+        if match:
+            hostname = match.group(1)
+            ip = match.group(2)
+            # Проверка, существует ли уже такая запись
+            if f"{ip}\t{hostname}" not in hosts_file:
+                hosts_list.append((ip, hostname))
+
+# Объединение файлов
+saver.merge_files(hosts_list, sys.argv[1:], backup_file)
--- saver.py
+++ saver.py
@@ -1,22 +1,22 @@
 import os
 import shutil
 
-
 def backuper(backup_file):
     if not os.path.isfile(backup_file):
         shutil.copy2("/etc/hosts", backup_file)
         print(f"Backup created: {backup_file}")
     else:
-        pass
+        print(f"Backup already exists: {backup_file}")
 
-
-def merge_files(hosts_list, path_to_hosts, backup_file):
+def merge_files(hosts_list, paths_to_hosts, backup_file):
+    # Восстановление из бэкапа перед объединением
     restore(backup_file)
     with open("/etc/hosts", "a") as f:
-        f.write(f"\n#\n# This file is merged with: {path_to_hosts}\n# The backup is located in: {backup_file}\n#\n")
+        for path_to_hosts in paths_to_hosts:
+            f.write(f"\n#\n# This file is merged with: {path_to_hosts}\n# The backup is located in: {backup_file}\n#\n")
         for ip, hostname in hosts_list:
             f.write(f"{ip}\t{hostname}\n")
 
-
 def restore(backup_file):
     shutil.copy2(backup_file, "/etc/hosts")
+    print(f"Restored backup: {backup_file}")
