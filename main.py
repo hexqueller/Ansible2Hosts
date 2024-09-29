80405170aa1d10274de96bebfa48bcbd1a121e69
Hexqueller <dima.nesed@gmail.com>
2024-09-29 22:46:26 +0300

    Убрал DEV инфу

--- main.py
+++ main.py
@@ -9,16 +9,13 @@ if len(sys.argv) < 2:
     print('Usage: python main.py <path_to_ansible_hosts_file1> <path_to_ansible_hosts_file2> ...')
     sys.exit(1)
 
-# Если первый аргумент "restore", то восстанавливаем бэкап и выходим
 if sys.argv[1] == "restore":
     saver.restore(backup_file)
     sys.exit(1)
 
-# Создание резервной копии
 saver.backuper(backup_file)
 
 hosts_list = []
-# Обработка всех переданных файлов
 for path_to_hosts in sys.argv[1:]:
     try:
         with open(path_to_hosts, 'r') as f:
@@ -27,7 +24,6 @@ for path_to_hosts in sys.argv[1:]:
         print(f"File not found: {path_to_hosts}")
         continue
 
-    # Чтение текущего содержимого файла /etc/hosts
     with open(backup_file, 'r') as f:
         hosts_file = f.read()
 
@@ -38,9 +34,8 @@ for path_to_hosts in sys.argv[1:]:
         if match:
             hostname = match.group(1)
             ip = match.group(2)
-            # Проверка, существует ли уже такая запись
             if f"{ip}\t{hostname}" not in hosts_file:
                 hosts_list.append((ip, hostname))
 
-# Объединение файлов
 saver.merge_files(hosts_list, sys.argv[1:], backup_file)
+
